1df1f2a5611435b0add7b8a58eef9ec86fc8cfd3
michele <0makoblaster0@gmail.com>
2022-12-07 18:17:32 +0100

    day06

---
+++ Day06.cs
@@ -0,0 +1,31 @@
+﻿namespace Advent2022;
+
+public class Day06 : BaseDay
+{
+    protected override string Part1()
+    {
+        var line = Input[0];
+        return ElaborateLine(line, 4);
+    }
+
+    public static string ElaborateLine(string line, int size)
+    {
+        for (var i = 0; i < line.Length; i++)
+        {
+            var end = i + size;
+            var chars = line[i..end];
+            if (chars.Distinct().Count() == chars.Length)
+            {
+                return end.ToString();
+            }
+        }
+
+        return "null";
+    }
+
+    protected override string Part2()
+    {
+        var line = Input[0];
+        return ElaborateLine(line, 14);
+    }
+}
---
+++ Day06Tests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace Advent2022;
+
+[TestFixture]
+public class Day06Tests
+{
+    [Test]
+    [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", ExpectedResult = "7")]
+    [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", ExpectedResult = "5")]
+    [TestCase("nppdvjthqldpwncqszvftbrmjlhg", ExpectedResult = "6")]
+    [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", ExpectedResult = "10")]
+    [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", ExpectedResult = "11")]
+    public string TestPart01BaseStrings(string input)
+    {
+        return Day06.ElaborateLine(input, 4);
+    }
+    
+    [Test]
+    [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", ExpectedResult = "19")]
+    [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", ExpectedResult = "23")]
+    [TestCase("nppdvjthqldpwncqszvftbrmjlhg", ExpectedResult = "23")]
+    [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", ExpectedResult = "29")]
+    [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", ExpectedResult = "26")]
+    public string TestPart02BaseStrings(string input)
+    {
+        return Day06.ElaborateLine(input, 14);
+    }
+}
